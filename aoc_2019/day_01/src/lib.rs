c07e4da926ba641deef482a9d1b63235f64ffda8
Loky HARK <loky.hark@pm.me>
2022-01-07 11:19:07 +0100

    AoC 2019 day 01

--- aoc_2019/day_01/src/lib.rs
+++ aoc_2019/day_01/src/lib.rs
@@ -3,10 +3,79 @@ use aoc::Result;
 pub const YEAR: u32 = 2019;
 pub const DAY: u32 = 1;
 
-pub fn part_one(input: &str) -> Result<usize> {
-    Ok(input.len())
+pub fn part_one(input: &str) -> Result<u32> {
+    let mut fuel = 0;
+    for mass in input.trim().lines() {
+        let mass = match mass.parse() {
+            Ok(mass) => mass,
+            Err(_) => return Err(format!("invalid mass: '{}'", mass).into()),
+        };
+        fuel += fuel_requirement(mass);
+    }
+    Ok(fuel)
 }
 
-pub fn part_two(input: &str) -> Result<usize> {
-    Ok(input.len())
+pub fn part_two(input: &str) -> Result<i32> {
+    let mut fuel = 0;
+    for mass in input.trim().lines() {
+        let mass = match mass.parse() {
+            Ok(mass) => mass,
+            Err(_) => return Err(format!("invalid mass: '{}'", mass).into()),
+        };
+        fuel += recursive_fuel_requirement(mass);
+    }
+    Ok(fuel)
+}
+
+fn fuel_requirement(mass: u32) -> u32 {
+    mass / 3 - 2
+}
+
+fn recursive_fuel_requirement(mass: i32) -> i32 {
+    let mut mass = mass;
+    let mut fuel = 0;
+    loop {
+        mass = mass / 3 - 2;
+        if mass > 0 {
+            fuel += mass
+        } else {
+            break;
+        }
+    }
+    fuel
+}
+
+#[test]
+fn part_one_example1() {
+    assert_eq!(fuel_requirement(12), 2);
+}
+
+#[test]
+fn part_one_example2() {
+    assert_eq!(fuel_requirement(14), 2);
+}
+
+#[test]
+fn part_one_example3() {
+    assert_eq!(fuel_requirement(1969), 654);
+}
+
+#[test]
+fn part_one_example4() {
+    assert_eq!(fuel_requirement(100756), 33583);
+}
+
+#[test]
+fn part_two_example1() {
+    assert_eq!(recursive_fuel_requirement(14), 2);
+}
+
+#[test]
+fn part_two_example2() {
+    assert_eq!(recursive_fuel_requirement(1969), 966);
+}
+
+#[test]
+fn part_rwo_example3() {
+    assert_eq!(recursive_fuel_requirement(100756), 50346);
 }
--- aoc_2019/day_01/tests/aoc_2019_day_01.rs
+++ aoc_2019/day_01/tests/aoc_2019_day_01.rs
@@ -5,13 +5,13 @@ use aoc_2019_day_01::*;
 #[test]
 fn part_one_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_one(input)?, input.len());
+    assert_eq!(part_one(input)?, 3256114);
     Ok(())
 }
 
 #[test]
 fn part_two_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_two(input)?, input.len());
+    assert_eq!(part_two(input)?, 4881302);
     Ok(())
 }
