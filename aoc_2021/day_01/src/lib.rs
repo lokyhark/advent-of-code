a5489f673fd4ed4ddcb54b198daf37ec41c1e5d4
Loky HARK <loky.hark@pm.me>
2022-01-07 11:34:45 +0100

    AoC 2021 day 01

--- aoc_2021/day_01/src/lib.rs
+++ aoc_2021/day_01/src/lib.rs
@@ -3,10 +3,56 @@ use aoc::Result;
 pub const YEAR: u32 = 2021;
 pub const DAY: u32 = 1;
 
-pub fn part_one(input: &str) -> Result<usize> {
-    Ok(input.len())
+pub fn part_one(input: &str) -> Result<u32> {
+    let mut report = Vec::new();
+    for line in input.trim().lines() {
+        let sweep = match line.parse::<u32>() {
+            Ok(sweep) => sweep,
+            Err(_) => return Err(format!("invalid sonar sweep: '{}'", line).into()),
+        };
+        report.push(sweep);
+    }
+    let mut increase = 0;
+    let mut iter = report.into_iter().peekable();
+    while let Some(sweep) = iter.next() {
+        match iter.peek() {
+            Some(peek) if *peek > sweep => increase += 1,
+            None => break,
+            _ => continue,
+        }
+    }
+    Ok(increase)
 }
 
-pub fn part_two(input: &str) -> Result<usize> {
-    Ok(input.len())
+pub fn part_two(input: &str) -> Result<u32> {
+    let mut report = Vec::new();
+    for line in input.trim().lines() {
+        let sweep = match line.parse::<u32>() {
+            Ok(sweep) => sweep,
+            Err(_) => return Err(format!("invalid sonar sweep: '{}'", line).into()),
+        };
+        report.push(sweep);
+    }
+    let mut increase = 0;
+    let mut iter = report.windows(3).peekable();
+    while let Some(window) = iter.next() {
+        match iter.peek() {
+            Some(peek) if peek.iter().sum::<u32>() > window.iter().sum() => increase += 1,
+            None => break,
+            _ => continue,
+        };
+    }
+    Ok(increase)
+}
+
+#[test]
+fn part_one_example() -> Result<()> {
+    assert_eq!(part_one("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n")?, 7);
+    Ok(())
+}
+
+#[test]
+fn part_two_example() -> Result<()> {
+    assert_eq!(part_two("199\n200\n208\n210\n200\n207\n240\n269\n260\n263\n")?, 5);
+    Ok(())
 }
--- aoc_2021/day_01/tests/aoc_2021_day_01.rs
+++ aoc_2021/day_01/tests/aoc_2021_day_01.rs
@@ -5,13 +5,13 @@ use aoc_2021_day_01::*;
 #[test]
 fn part_one_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_one(input)?, input.len());
+    assert_eq!(part_one(input)?, 1602);
     Ok(())
 }
 
 #[test]
 fn part_two_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_two(input)?, input.len());
+    assert_eq!(part_two(input)?, 1633);
     Ok(())
 }
