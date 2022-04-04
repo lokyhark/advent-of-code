44fe3a697c87a9c5ca0fcf652517ab8d7402f50b
Loky HARK <loky.hark@pm.me>
2022-04-04 17:44:00 +0200

    AoC 2015 day 17

--- aoc_2015/day_17/tests/aoc_2015_day_17.rs
+++ aoc_2015/day_17/tests/aoc_2015_day_17.rs
@@ -5,13 +5,13 @@ use aoc_2015_day_17::*;
 #[test]
 fn part_one_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_one(input)?, input.len());
+    assert_eq!(part_one(input)?, 1304);
     Ok(())
 }
 
 #[test]
 fn part_two_answer() -> Result<()> {
     let input = include_str!("../input/input.txt");
-    assert_eq!(part_two(input)?, input.len());
+    assert_eq!(part_two(input)?, 18);
     Ok(())
 }
