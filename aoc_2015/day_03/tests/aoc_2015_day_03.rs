d127674532545055eafea2f5d71ffea06d68211c
Loky HARK <loky.hark@pm.me>
2022-10-26 22:55:19 +0200

    Aoc 2015 day 03

---
+++ aoc_2015/day_03/tests/aoc_2015_day_03.rs
@@ -0,0 +1,17 @@
+use aoc::Result;
+
+use aoc_2015_day_03::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_one(input)?, 2081);
+    Ok(())
+}
+
+#[test]
+fn part_two_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_two(input)?, 2341);
+    Ok(())
+}
