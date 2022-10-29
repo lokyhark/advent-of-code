cce33def397fbebf69ee02bbebd3439d17a44f26
Loky HARK <loky.hark@pm.me>
2022-10-29 23:38:34 +0200

    Aoc 2016 day 06

---
+++ aoc_2016/day_06/tests/aoc_2016_day_06.rs
@@ -0,0 +1,17 @@
+use aoc::Result;
+
+use aoc_2016_day_06::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_one(input)?, "qzedlxso");
+    Ok(())
+}
+
+#[test]
+fn part_two_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_two(input)?, "ucmifjae");
+    Ok(())
+}
