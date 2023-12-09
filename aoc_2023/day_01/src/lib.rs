57125220e87f8d1dfc93d14af536693205a60cfe
Loky HARK <loky.hark@protonmail.com>
2023-12-09 15:41:26 +0100

    Aoc 2023 day 01

---
+++ aoc_2023/day_01/src/lib.rs
@@ -0,0 +1,96 @@
+use aoc::Result;
+
+pub const YEAR: u32 = 2023;
+pub const DAY: u32 = 1;
+
+pub fn part_one(input: &str) -> Result<u32> {
+    let mut sum = 0;
+    for line in input.lines() {
+        let left = line.chars().find(|x| x.is_ascii_digit()).map(|x| x.to_digit(10).unwrap()).expect("no digit found");
+        let right = line.chars().rev().find(|x| x.is_ascii_digit()).map(|x| x.to_digit(10).unwrap()).expect("no digit found");
+        let val = left * 10 + right;
+        sum += val;
+    }
+    Ok(sum)
+}
+
+#[rustfmt::skip]
+const PATTERNS: [&str; 18] = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+
+pub fn part_two(input: &str) -> Result<u32> {
+    let mut sum = 0;
+    for line in input.lines() {
+        // Left
+        let mut digit = None;
+        let mut pos = line.len();
+        for pattern in PATTERNS {
+            match line.find(pattern) {
+                Some(idx) => {
+                    if idx < pos {
+                        digit = Some(pattern);
+                        pos = idx;
+                    }
+                }
+                _ => continue,
+            }
+        }
+        let left = digit.expect("digit not found");
+
+        // Right
+        let mut digit = None;
+        let mut pos = 0;
+        for pattern in PATTERNS {
+            match line.rfind(pattern) {
+                Some(idx) => {
+                    if idx > pos {
+                        digit = Some(pattern);
+                        pos = idx;
+                    }
+                }
+                _ => continue,
+            }
+        }
+        let right = digit.unwrap_or(left);
+
+        // Parse
+        let left = parse_digit(left);
+        let right = parse_digit(right);
+
+        // Sum
+        let val = left * 10 + right;
+        sum += val;
+    }
+    Ok(dbg!(sum))
+}
+
+pub fn parse_digit(digit: &str) -> u32 {
+    match digit.as_bytes() {
+        b"1" | b"one" => 1,
+        b"2" | b"two" => 2,
+        b"3" | b"three" => 3,
+        b"4" | b"four" => 4,
+        b"5" | b"five" => 5,
+        b"6" | b"six" => 6,
+        b"7" | b"seven" => 7,
+        b"8" | b"eight" => 8,
+        b"9" | b"nine" => 9,
+        _ => panic!("invalid digit"),
+    }
+}
+
+#[test]
+fn part_one_example() -> Result<()> {
+    let input = include_str!("../input/example1.txt");
+    assert_eq!(part_one(input)?, 142);
+    Ok(())
+}
+
+#[test]
+fn part_two_example() -> Result<()> {
+    let input = include_str!("../input/example2.txt");
+    assert_eq!(part_two(input)?, 281);
+    Ok(())
+}
---
+++ src/bin/setup.rs
@@ -0,0 +1,206 @@
+use std::{fs::File, path::Path};
+
+use anyhow::Result;
+use isahc::{HttpClient, ReadResponseExt};
+
+fn main() -> Result<()> {
+    let cookie = std::env::args().nth(1).expect("no cookie argument");
+    let client = client(&cookie)?;
+    let root = std::env::current_dir().unwrap();
+    for year in 2023..=2023 {
+        for day in 1..=1 {
+            setup_layout(&root, &client, year, day)?;
+        }
+    }
+    Ok(())
+}
+
+fn client(cookie: &str) -> Result<HttpClient> {
+    let cookie = format!("session={}", cookie);
+    let client = HttpClient::builder().default_header("Cookie", cookie).build()?;
+    Ok(client)
+}
+
+fn setup_layout(root: &Path, client: &HttpClient, year: u32, day: u32) -> Result<()> {
+    let path = root.join(format!("aoc_{}/day_{:02}", year, day));
+    if !path.exists() {
+        let url = format!("https://adventofcode.com/{}/day/{}", year, day);
+        let response = client.get(&url)?;
+        if response.status().is_success() {
+            let crate_name = format!("aoc_{}_day_{:02}", year, day);
+            dir(&path)?;
+            cargo(&path, &crate_name, year, day)?;
+            src_dir(&path)?;
+            bin(&path, &crate_name)?;
+            lib(&path, year, day)?;
+            tests_dir(&path)?;
+            test(&path, &crate_name)?;
+            readme(&path, client, year, day)?;
+            input_dir(&path)?;
+            input(&path, client, year, day)?;
+        }
+    }
+    Ok(())
+}
+
+fn dir(path: &Path) -> Result<()> {
+    std::fs::create_dir_all(path)?;
+    Ok(())
+}
+
+fn cargo(path: &Path, crate_name: &str, year: u32, day: u32) -> Result<()> {
+    let path = path.join("Cargo.toml");
+    let contents = format!(
+        "[package]\n\
+        name = \"{}\"\n\
+        version = \"1.0.0\"\n\
+        edition = \"2021\"\n\
+        description = \"Advent of Code {}-{}\"\n\
+        license = \"MIT\"\n\
+        \n\
+        [dependencies]\n\
+        aoc = {{ path = \"../..\" }}\n",
+        crate_name, year, day
+    );
+    std::fs::write(path, contents)?;
+    Ok(())
+}
+
+fn src_dir(path: &Path) -> Result<()> {
+    let path = path.join("src");
+    std::fs::create_dir_all(path)?;
+    Ok(())
+}
+
+fn bin(path: &Path, crate_name: &str) -> Result<()> {
+    let path = path.join("src/main.rs");
+    let contents = format!(
+        "\
+use std::io::{{stdout, Write}};
+
+use aoc::Result;
+
+use {}::*;
+
+fn main() -> Result<()> {{
+    println!(\"Advent of Code {{}}-{{:02}}\", YEAR, DAY);
+    let input = aoc::input_from_stdin()?;
+    let answer = part_one(&input)?;
+    writeln!(stdout(), \"--> part one:\")?;
+    writeln!(stdout(), \"{{}}\", answer)?;
+    let answer = part_two(&input)?;
+    writeln!(stdout(), \"--> part two:\")?;
+    writeln!(stdout(), \"{{}}\", answer)?;
+    Ok(())
+}}
+",
+        crate_name,
+    );
+    std::fs::write(path, contents)?;
+    Ok(())
+}
+
+fn lib(path: &Path, year: u32, day: u32) -> Result<()> {
+    let path = path.join("src/lib.rs");
+    let contents = format!(
+        "\
+use aoc::Result;
+
+pub const YEAR: u32 = {};
+pub const DAY: u32 = {};
+
+pub fn part_one(input: &str) -> Result<usize> {{
+    Ok(input.len())
+}}
+
+pub fn part_two(input: &str) -> Result<usize> {{
+    Ok(input.len())
+}}
+
+#[test]
+fn part_one_example() -> Result<()> {{
+    Ok(())
+}}
+
+#[test]
+fn part_two_example() -> Result<()> {{
+    Ok(())
+}}
+",
+        year, day
+    );
+    std::fs::write(path, contents)?;
+    Ok(())
+}
+
+fn tests_dir(path: &Path) -> Result<()> {
+    let path = path.join("tests");
+    std::fs::create_dir_all(path)?;
+    Ok(())
+}
+
+fn test(path: &Path, crate_name: &str) -> Result<()> {
+    let mut path = path.join("tests");
+    path.push(crate_name);
+    path.set_extension("rs");
+    let contents = format!(
+        "\
+use aoc::Result;
+
+use {}::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {{
+    let input = include_str!(\"../input/input.txt\");
+    assert_eq!(part_one(input)?, input.len());
+    Ok(())
+}}
+
+#[test]
+fn part_two_answer() -> Result<()> {{
+    let input = include_str!(\"../input/input.txt\");
+    assert_eq!(part_two(input)?, input.len());
+    Ok(())
+}}
+",
+        crate_name,
+    );
+    std::fs::write(path, contents)?;
+    Ok(())
+}
+
+fn readme(path: &Path, client: &HttpClient, year: u32, day: u32) -> Result<()> {
+    let path = path.join("README.md");
+    let url = format!("https://adventofcode.com/{}/day/{}", year, day);
+    let mut response = client.get(&url)?;
+    if response.status().is_success() {
+        let text = response.text()?;
+        let mut split = text.split("---");
+        let title = split.nth(1).unwrap().trim();
+        let contents = format!(
+            "\
+# Advent of Code -- Year {} - Day {:02}
+
+[{}]({})
+",
+            year, day, title, url
+        );
+        std::fs::write(path, contents)?;
+    }
+    Ok(())
+}
+
+fn input_dir(path: &Path) -> Result<()> {
+    let path = path.join("input");
+    std::fs::create_dir_all(path)?;
+    Ok(())
+}
+
+fn input(path: &Path, client: &HttpClient, year: u32, day: u32) -> Result<()> {
+    let path = path.join("input/input.txt");
+    let mut file = File::create(path)?;
+    let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
+    let mut response = client.get(url)?;
+    response.copy_to(&mut file)?;
+    Ok(())
+}
