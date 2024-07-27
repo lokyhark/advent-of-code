0e3676cce872e117016b8c05345c476cf3ed5055
Loky HARK <loky.hark@pm.me>
2024-07-27 17:29:07 +0200

    Aoc 2022 day 04

---
+++ aoc_2022/day_04/src/lib.rs
@@ -0,0 +1,88 @@
+use aoc::Result;
+
+pub const YEAR: u32 = 2022;
+pub const DAY: u32 = 4;
+
+pub fn part_one(input: &str) -> Result<u32> {
+    let mut count = 0;
+    for line in input.lines() {
+        let section = parse_section(line)?;
+        if section.contain() {
+            count += 1;
+        }
+    }
+    Ok(count)
+}
+
+pub fn part_two(input: &str) -> Result<usize> {
+    let mut count = 0;
+    for line in input.lines() {
+        let section = parse_section(line)?;
+        if section.overlap() {
+            count += 1;
+        }
+    }
+    Ok(count)
+}
+
+fn parse_section(slice: &str) -> Result<Section> {
+    let mut split = slice.split(',');
+    let left = match split.next() {
+        Some(slice) => parse_range(slice)?,
+        _ => return Err("invalid section".into()),
+    };
+    let right = match split.next() {
+        Some(slice) => parse_range(slice)?,
+        _ => return Err("invalid section".into()),
+    };
+    Ok(Section { left, right })
+}
+
+fn parse_range(slice: &str) -> Result<Range> {
+    let mut split = slice.split('-');
+    let start = match split.next().map(|x| x.parse::<u32>()) {
+        Some(Ok(start)) => start,
+        _ => return Err("invalid range".into()),
+    };
+    let end = match split.next().map(|x| x.parse::<u32>()) {
+        Some(Ok(end)) => end,
+        _ => return Err("invalid range".into()),
+    };
+    Ok(Range { start, end })
+}
+
+#[derive(Debug)]
+struct Section {
+    left: Range,
+    right: Range,
+}
+
+impl Section {
+    fn contain(&self) -> bool {
+        (self.left.start >= self.right.start && self.left.end <= self.right.end) || (self.right.start >= self.left.start && self.right.end <= self.left.end)
+    }
+
+    fn overlap(&self) -> bool {
+        self.left.end >= self.right.start && self.left.start <= self.right.end
+    }
+}
+
+#[derive(Debug)]
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+#[test]
+fn part_one_example() -> Result<()> {
+    let input = include_str!("../input/example.txt");
+    assert_eq!(part_one(input)?, 2);
+    Ok(())
+}
+
+#[test]
+fn part_two_example() -> Result<()> {
+    let input = include_str!("../input/example.txt");
+    assert_eq!(part_two(input)?, 4);
+    Ok(())
+}
---
+++ aoc_2022/day_04/tests/aoc_2022_day_04.rs
@@ -0,0 +1,17 @@
+use aoc::Result;
+
+use aoc_2022_day_04::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_one(input)?, 536);
+    Ok(())
+}
+
+#[test]
+fn part_two_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_two(input)?, 845);
+    Ok(())
+}
