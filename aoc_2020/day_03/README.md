f1a1105052fcdc3d95b84f6065b5ea02a070fbc3
Loky HARK <loky.hark@pm.me>
2024-11-01 14:58:40 +0100

    Aoc 2020 day 03

---
+++ aoc_2020/day_03/README.md
@@ -0,0 +1,3 @@
+# Advent of Code -- Year 2020 - Day 03
+
+[Day 3: Toboggan Trajectory](https://adventofcode.com/2020/day/3)
---
+++ aoc_2020/day_03/tests/aoc_2020_day_03.rs
@@ -0,0 +1,17 @@
+use aoc::Result;
+
+use aoc_2020_day_03::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_one(input)?, 187);
+    Ok(())
+}
+
+#[test]
+fn part_two_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_two(input)?, 4723283400);
+    Ok(())
+}
