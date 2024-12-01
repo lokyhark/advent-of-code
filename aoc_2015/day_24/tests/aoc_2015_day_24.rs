3f1fb3335258d0f14c767636bf9320828394c6d4
Loky HARK <loky.hark@pm.me>
2024-12-01 13:52:22 +0100

    Aoc 2015 day 24

---
+++ aoc_2015/day_24/tests/aoc_2015_day_24.rs
@@ -0,0 +1,17 @@
+use aoc::*;
+
+use aoc_2015_day_24::*;
+
+#[test]
+fn part_one_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_one(input)?, 10723906903);
+    Ok(())
+}
+
+#[test]
+fn part_two_answer() -> Result<()> {
+    let input = include_str!("../input/input.txt");
+    assert_eq!(part_two(input)?, 74850409);
+    Ok(())
+}
